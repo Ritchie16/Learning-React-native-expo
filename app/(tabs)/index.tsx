5713942a1fcd9485f69116718cc39059dd5e8482
Richard Munthali <richardmunthali016@gmail.com>
2025-12-26 17:15:22 +0200

    add image picker function in index.tsx and pass as a prop to Button.tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,6 @@
 import Button from '@/components/Button';
 import ImageViewer from "@/components/ImageViewer";
+import * as ImagePicker from 'expo-image-picker';
 import React from "react";
 import { StyleSheet, View } from "react-native";
 
@@ -8,6 +9,23 @@ const PlaceholderImage = require('@/assets/images/background-image.png');
 
 
 export default function Index() {
+  const pickImageAsync = async () => {
+    // No permissions request is necessary for launching the image library
+    //the function launchImageLibraryAsync opens the image gallery
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ['images'], //only images
+      allowsEditing: true, //allow user to edit the image
+      quality: 1, //highest quality and other quality values are 0 to 1
+    });
+
+    //if the user did not cancel the process
+    if(!result.canceled){
+      console.log(result);
+    }
+    else {
+      alert('You did not select any image.');
+    }
+  }
   return (
     <View
       style={styles.container}
@@ -17,7 +35,8 @@ export default function Index() {
       </View>
       
       <View style={styles.footerContainer} >
-        <Button label="Choose a photo" theme='primary' />
+        {/**passing the props including a prop to select an image to Pressable */}
+        <Button label="Choose a photo" theme='primary' onPress={pickImageAsync}/>
         <Button label="Use this photo" />
       </View>
     </View>
