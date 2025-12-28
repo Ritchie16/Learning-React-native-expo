f206cafd43db920777ae973159a2c8fdd244974b
Richard Munthali <richardmunthali016@gmail.com>
2025-12-29 00:46:18 +0200

    fix(web): cast imageRef.current to HTMLElement to resolve TypeScript error

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -106,7 +106,7 @@ export default function Index() {
       }
     } else {
       try {
-        const dataUrl = await domtoimage.toJpeg(imageRef.current , {
+        const dataUrl = await domtoimage.toJpeg(imageRef.current as unknown as HTMLElement, {
         quality: 0.95,
         width: 320,
         height: 440,
